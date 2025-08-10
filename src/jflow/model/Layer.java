20fe49080fa0a4c95e3009dc298266b309fb9e3d
Nathan Yasnovsky <nathanyasnovsky@gmail.com>
2025-08-10 10:47:43 -0700

    fix(Layer): last layer should always store output for access in loss calculation.
    fixes: NullPointerException in Sequential.train(...)

--- src/jflow/model/Layer.java
+++ src/jflow/model/Layer.java
@@ -93,7 +93,7 @@ public abstract class Layer {
 
     protected JMatrix trackOutput(JMatrix output, boolean training) {
         this.outputShape = output.shape();
-        if (training || debugEnabled) {
+        if (training || debugEnabled || getLayerList().getLast().equals(this)) { // Last layer should store output for convenience
             this.output = output;
         } else {
             // Ensure memory is freed
