e561445236c9568a77960f1a51f560e9fee8fd23
Nathan Yasnovsky <nathanyasnovsky@gmail.com>
2025-09-30 17:12:54 -0700

    fix(Layer): bug fix in accessing the last layer output

--- src/jflow/model/Layer.java
+++ src/jflow/model/Layer.java
@@ -7,7 +7,7 @@ import jflow.utils.Callbacks;
 public abstract class Layer {
     // Layer graph
     private LayerList layers;
-    private Layer firstLayer; // Cached for performance
+    private Layer lastLayer; // Cached for performance
 
     // Data
     private JMatrix output;
@@ -35,7 +35,7 @@ public abstract class Layer {
 
     protected void link(LayerList layers) {
         this.layers = layers;
-        this.firstLayer = layers.getFirst();
+        lastLayer = layers.getLast();
     }
 
     protected LayerList getLayerList() {
@@ -95,7 +95,7 @@ public abstract class Layer {
 
     protected JMatrix trackOutput(JMatrix output, boolean training) {
         this.outputShape = output.shape();
-        if (training || debugEnabled || firstLayer.equals(this)) { // Last layer should store output for convenience
+        if (training || debugEnabled || lastLayer.equals(this)) { // Last layer should store output
             this.output = output;
         } else {
             // Ensure memory is freed
